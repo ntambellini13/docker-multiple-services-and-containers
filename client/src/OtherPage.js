61e91abf679edf5556143cf81204d86e581ddc40
ntambellini13 <40220159+ntambellini13@users.noreply.github.com>
2020-05-18 10:28:23 -0600

    initial commit

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => {
-    return (
-        <div>
-            I am another page!
-            <Link to='/'>Home</Link>
-        </div>
-    );
+  return (
+    <div>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
 };
