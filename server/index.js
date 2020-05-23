c4c175ab28dc5b7f38e8320847e1651cc594dfaf
Nico Tambellini <ntambellini13@gmail.com>
2020-05-23 09:31:57 -0600

    changed server index.js

--- server/index.js
+++ server/index.js
@@ -19,11 +19,10 @@ const pgClient = new Pool({
   port: keys.pgPort
 });
 
-pgClient.on('connect', () => {
-  pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
-});
+pgClient.on('error', () => console.log('Lost PG connection'));
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
